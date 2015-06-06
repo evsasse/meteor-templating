d4c2e99c46b2fe46322915c54e47b1bba5b1afe2
unknown <evsasse@gmail.com>
2015-06-06 20:50:15 -0300

    Fixed relative to fixed, changed the way the css is altered by js

--- client/views/navigation/navigation.js
+++ client/views/navigation/navigation.js
@@ -1,13 +1,11 @@
-Template.navigation.helpers({
-  open: function(){
-    if(Session.get('navigationOpen') === true)
-      return 'open';
-  }
-})
-
 Template.navigation.events({
   'click .toggleNavigation': function(event){
     var current = Session.get('navigationOpen') === true;
 		Session.set('navigationOpen',!current);
+
+    if(!current)
+      document.body.classList.add('navigationOpen')
+    else
+      document.body.classList.remove('navigationOpen')
   }
 })
