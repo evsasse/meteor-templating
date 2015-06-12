bbd3f3d85435e9e5fb962b0b51209c8f2480443e
evsasse <evsasse@gmail.com>
2015-06-12 11:32:09 -0300

    Hide sidemenu button from header when its open, changed the way body's background work, added some style to main

--- client/views/navigation/navigation.js
+++ client/views/navigation/navigation.js
@@ -1,13 +1,15 @@
+function toggleNavigation(){
+  var current = Session.get('navigation-open') === true;
+  Session.set('navigation-open',!current);
+
+  if(!current)
+    document.body.classList.add('navigation-open')
+  else
+    document.body.classList.remove('navigation-open')
+}
+
 Template.navigation.events({
   'click .toggle-navigation': function(event){
-    var current = Session.get('navigation-open') === true;
-		Session.set('navigation-open',!current);
-
-    console.log(current);
-
-    if(!current)
-      document.body.classList.add('navigation-open')
-    else
-      document.body.classList.remove('navigation-open')
+    toggleNavigation();
   }
 })
